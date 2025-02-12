e883fbbd31d83b980728749edd39a61df660ed42
Toshiki <bushy.trivial.0o@icloud.com>
2025-02-12 11:38:16 +0900

    Remove App.css and update root.tsx import

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
