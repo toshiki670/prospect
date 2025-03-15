042ab9231f87dd35a9d7259147fbdd87dceb0682
Toshiki <bushy.trivial.0o@icloud.com>
2025-03-16 01:37:46 +0900

    Refactor EnglishName and JapaneseName to use Option<String>
    - Updated EnglishName and JapaneseName structs to use Option<String> for improved handling of nullable values.
    - Added From implementation for both structs to facilitate conversion from Option<String> to their respective types.

--- src-tauri/src/domain/tokyo_stock_exchange/english_name.rs
+++ src-tauri/src/domain/tokyo_stock_exchange/english_name.rs
@@ -1,10 +1,16 @@
 #[derive(Default, Debug, Clone, PartialEq, Eq)]
-pub struct EnglishName(String);
+pub struct EnglishName(Option<String>);
 
 impl core::ops::Deref for EnglishName {
-    type Target = String;
+    type Target = Option<String>;
 
     fn deref(&'_ self) -> &'_ Self::Target {
         &self.0
+    }
+}
+
+impl From<Option<String>> for EnglishName {
+    fn from(value: Option<String>) -> Self {
+        Self(value)
     }
 }
--- src-tauri/src/domain/tokyo_stock_exchange/japanese_name.rs
+++ src-tauri/src/domain/tokyo_stock_exchange/japanese_name.rs
@@ -1,10 +1,16 @@
 #[derive(Default, Debug, Clone, PartialEq, Eq)]
-pub struct JapaneseName(String);
+pub struct JapaneseName(Option<String>);
 
 impl core::ops::Deref for JapaneseName {
-    type Target = String;
+    type Target = Option<String>;
 
     fn deref(&'_ self) -> &'_ Self::Target {
         &self.0
+    }
+}
+
+impl From<Option<String>> for JapaneseName {
+    fn from(value: Option<String>) -> Self {
+        Self(value)
     }
 }
