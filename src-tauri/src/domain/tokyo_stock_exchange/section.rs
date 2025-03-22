7add9f590f72225e2be0cf755f617074e32492e4
Toshiki <bushy.trivial.0o@icloud.com>
2025-03-22 17:39:36 +0900

    Add section documentation for Tokyo Stock Exchange
    - Added detailed comments for various market types and categories within the Section struct.
    - This change improves code readability and provides clarity on the different financial instruments represented in the Tokyo Stock Exchange.

--- src-tauri/src/domain/tokyo_stock_exchange/section.rs
+++ src-tauri/src/domain/tokyo_stock_exchange/section.rs
@@ -1,6 +1,28 @@
 #[derive(Default, Debug, Clone, PartialEq, Eq)]
 pub struct Section(String);
 
+/// ETF・ETN
+/// PRO Market
+/// REIT・ベンチャーファンド・カントリーファンド・インフラファンド
+/// グロース（内国株式）
+/// グロース（外国株式）
+/// スタンダード（内国株式）
+/// スタンダード（外国株式）
+/// プライム（内国株式）
+/// プライム（外国株式）
+/// 出資証券
+
+// Equity Contribution Securities
+// ETFs/ ETNs
+// Growth Market (Foreign)
+// Growth Market(Domestic)
+// Prime Market (Domestic)
+// Prime Market(Foreign)
+// PRO Market
+// REIT, Venture Funds, Country Funds and Infrastructure Funds
+// Standard Market(Domestic)
+// Standard Market(Foreign)
+
 impl core::ops::Deref for Section {
     type Target = String;
 
